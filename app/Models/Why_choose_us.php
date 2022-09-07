52bc4a1bcd0eaca431f86c2cac27afa28b365af6
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-09-07 09:11:27 +0430

    Tables Created

---
+++ app/Models/Why_choose_us.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Why_choose_us extends Model
+{
+    use HasFactory;
+
+    
+    protected $guarded = [];
+}
