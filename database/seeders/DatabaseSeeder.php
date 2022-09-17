fdb0add64a6ac30b7899ed38f995d51f4503fb30
Hanifullah Jamalzai <hanifullahjamalzai2014@gmail.com>
2022-09-17 09:57:36 +0430

    category factory done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Category;
 use App\Models\Why_choose_us;
 use Illuminate\Database\Seeder;
 
@@ -23,6 +24,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        Why_choose_us::factory()->count(3)->create();
+        // Why_choose_us::factory()->count(3)->create();
+        Category::factory()->count(3)->create();
+
     }
 }
